702d1210ee21ceba37b7fcaab6d0e77c27a0bd4d
hcampbell1212 <heather.campbell1212@gmail.com>
2021-02-23 11:22:17 -0600

    VariablesExercise Qs 1-8

---
+++ variablesExercise.js
@@ -0,0 +1,39 @@
+// 1
+// const locations = 2;
+// console.log(locations);
+
+//  2
+let num = 20;
+console.log(num);
+
+//  3
+num = num -5;
+console.log(num);
+
+//  4
+num += 7;
+console.log(num);
+
+//  5
+num --;
+console.log(num)
+
+// 6
+num = ("This is not a number")
+console.log(num)
+
+// 7
+let blank;
+console.log(blank);
+
+//  8
+blank =  ("blank");
+console.log(blank);
+
+//  9
+blank = null;
+console.log(blank);
+
+// BONUS
+
+
